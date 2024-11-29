4116ac8a777090a17eb2b566f6628ef4285a45c2
Priyatosh Dixit <priyatoshdixit3@gmail.com>
2024-11-30 03:53:17 +0530

    Add networking utility * add core package * modularized code moved code in feature level package

---
+++ app/src/main/java/com/priyatosh/cryptotracker/crypto/presentation/coin_list/CoinListState.kt
@@ -0,0 +1,12 @@
+package com.priyatosh.cryptotracker.crypto.presentation.coin_list
+
+import androidx.compose.runtime.Immutable
+import com.priyatosh.cryptotracker.crypto.domain.Coin
+import com.priyatosh.cryptotracker.crypto.presentation.models.CoinUi
+
+@Immutable  //Signifies that this class itself never changes, if it changes the whole instance is replaced
+data class CoinListState(
+    val isLoading: Boolean = false,
+    val coins: List<CoinUi> = emptyList(),
+    val selectedCoin: Coin? = null,
+)
