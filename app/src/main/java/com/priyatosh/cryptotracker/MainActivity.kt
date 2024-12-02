af9616eb82198d559fddc5444bd700937ec85d04
Priyatosh Dixit <priyatoshdixit3@gmail.com>
2024-12-03 02:09:11 +0530

    Add Koin Dependency Injection & display coin list

--- app/src/main/java/com/priyatosh/cryptotracker/MainActivity.kt
+++ app/src/main/java/com/priyatosh/cryptotracker/MainActivity.kt
@@ -9,9 +9,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.priyatosh.cryptotracker.crypto.presentation.coin_list.CoinListScreen
+import com.priyatosh.cryptotracker.crypto.presentation.coin_list.CoinListViewModel
 import com.priyatosh.cryptotracker.ui.theme.CryptoTrackerTheme
+import org.koin.androidx.compose.koinViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +26,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             CryptoTrackerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    val viewmodel = koinViewModel<CoinListViewModel>()
+                    val state by viewmodel.state.collectAsStateWithLifecycle()
+                    
+                    CoinListScreen(
+                        state = state,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CryptoTrackerTheme {
-        Greeting("Android")
     }
 }
