93a0b26f7be373b4a0430646295e8dd0adfac6c8
Priyatosh Dixit <priyatoshdixit3@gmail.com>
2024-12-04 02:44:12 +0530

    Add coin detail screen * add ui and component for coin detail screen * add action in the list screen

--- app/src/main/java/com/priyatosh/cryptotracker/MainActivity.kt
+++ app/src/main/java/com/priyatosh/cryptotracker/MainActivity.kt
@@ -8,16 +8,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.priyatosh.cryptotracker.core.presentation.util.ObserveAsEvents
 import com.priyatosh.cryptotracker.core.presentation.util.toString
+import com.priyatosh.cryptotracker.crypto.presentation.coin_detail.CoinDetailScreen
 import com.priyatosh.cryptotracker.crypto.presentation.coin_list.CoinListEvent
 import com.priyatosh.cryptotracker.crypto.presentation.coin_list.CoinListScreen
 import com.priyatosh.cryptotracker.crypto.presentation.coin_list.CoinListViewModel
@@ -44,11 +41,22 @@ class MainActivity : ComponentActivity() {
                             ).show()
                         }
                     }
-                    
-                    CoinListScreen(
-                        state = state,
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+                    when {
+                        state.selectedCoin != null -> {
+                            CoinDetailScreen(
+                                state = state,
+                                modifier = Modifier.padding(innerPadding)
+                            )
+                        }
+                        else -> {
+                            CoinListScreen(
+                                state = state,
+                                onAction = viewmodel::onAction,
+                                modifier = Modifier.padding(innerPadding)
+                            )
+                        }
+                    }
                 }
             }
         }
